e85c4e4b3deb8c6523c624dc6955a2f582048def
Renzo Stefano <remesap@unal.edu.co>
2020-05-19 10:34:09 -0500

    cuántos y cuáles clusters

--- hptahk.cpp
+++ hptahk.cpp
@@ -17,8 +17,8 @@ int n_labels = 0;
 
 int main(void)
 {
-  const int L = 30;
-  double p = 0.7;
+  const int L = 1000;
+  double p = 0.5;
   Eigen::MatrixXi MATRIX(L, L);
   set_M(MATRIX, p);
   initialize(L*L/2);
@@ -151,69 +151,117 @@ int percolant_cluster_finder(Eigen::MatrixXi & M, int size)
 {
   std::vector<int> rowlabels0{};
   std::vector<int> rowlabelsL{};
-  std::vector<int> percolantes1{};
-  std::vector<int> percolantes2{};
+  std::vector<int> collabels0{};
+  std::vector<int> collabelsL{};
+  std::vector<int> prows1{};
+  std::vector<int> prows2{};
+  std::vector<int> pcols1{};
+  std::vector<int> pcols2{};
+  std::vector<int> percolantes{};
   
-  //extracción de los elementos de las filas
+  //extracción de los elementos de los extremos
   for (int j = 0; j < M.cols(); ++j)
     {
-      if(M(0,j) != 0)
+      //filas 
+      if(M(0, j) != 0)
 	rowlabels0.push_back(M(0,j));
       
       if(M(size-1, j) != 0)
 	rowlabelsL.push_back(M(size-1,j));
+
+      //columnas 
+      if(M(j, 0) != 0)
+	collabels0.push_back(M(j, 0));
+      
+      if(M(j, size-1) != 0)
+	collabelsL.push_back(M(j, size-1));
+
     }
 
-  for (int j = 0; j < rowlabels0.size(); ++j)
-    std::cout << rowlabels0[j] << "  ";
-
-  std::cout << "\n\v";
-  
-  for (int j = 0; j < rowlabelsL.size(); ++j)
-    std::cout << rowlabelsL[j] << "  ";
-
   //emparejamiento (no elimina elementos repetidos)
+  
   for (int i = 0; i < rowlabels0.size(); ++i)
     for (int j = 0; j < rowlabelsL.size(); ++j)
       {
 	if ( rowlabels0[i] == rowlabelsL[j] )
-	  percolantes1.push_back(rowlabels0[i]);
+	  prows1.push_back(rowlabels0[i]);
       }
+
   
+  for (int i = 0; i < collabels0.size(); ++i)
+    for (int j = 0; j < collabelsL.size(); ++j)
+      {
+	if ( collabels0[i] == collabelsL[j] )
+	  pcols1.push_back(collabels0[i]);
+      }
 
-  std::cout << "\n\v";
-
-  for (int j = 0; j < percolantes1.size(); ++j)
-    std::cout << percolantes1[j] << "  ";
-
-  std::cout << "\n\v";
-    
-  //hay percolantes?
   
-  if ( percolantes1.size() > 0)
+  //hay percolantes en las filas?
+  
+  if ( prows1.size() > 0)
     {
-//eliminación de los elementos repetidos
+      //eliminación de los elementos repetidos
       
-      percolantes2.push_back( percolantes1[0] );
+      prows2.push_back( prows1[0] );
       
-      for (int j = 1; j < percolantes1.size(); ++j)
+      for (int j = 1; j < prows1.size(); ++j)
 	{
-	  if ( percolantes1[j] != percolantes2.back())  //comparando con el último elemento del vector 
-	    percolantes2.push_back( percolantes1[j] );
+	  if ( prows1[j] != prows2.back())  //comparando con el último elemento del vector 
+	    prows2.push_back( prows1[j] );
 	}
       
-      for (int j = 0; j < percolantes2.size(); ++j)
-	std::cout << percolantes2[j] << "  ";
+      for (int j = 0; j < prows2.size(); ++j)
+	std::cout << prows2[j] << " prows2 ";  //*******
     }
-
+  
   else
-    std::cout << "nada de nada  ";
-  
-  
-  std::cout << percolantes2.size();
+    std::cout << "nada de nada en las filas :'v ";
 
-  return 0;
+  std::cout << "\n\v";
+  //hay percolantes en las columnas?
   
+  if ( pcols1.size() > 0)
+    {
+      //eliminación de los elementos repetidos
+      
+      pcols2.push_back( pcols1[0] );
+      
+      for (int j = 1; j < pcols1.size(); ++j)
+	{
+	  if ( pcols1[j] != pcols2.back())  //comparando con el último elemento del vector 
+	    pcols2.push_back( pcols1[j] );
+	}
+      
+      for (int j = 0; j < pcols2.size(); ++j)
+	std::cout << pcols2[j] << "  pcols2";
+    }
+  
+  else
+    std::cout << "nada de nada en las columnas :'v ";
+
+  
+  if ( prows2.size() > 0 || pcols2.size() > 0)
+    {
+      //hasta el momento tenemos los vectores prows2 y pcols2, los vamos a fusionar en el vector percolantes:
+      prows2.insert( prows2.end(), pcols2.begin(), pcols2.end() );
+      percolantes.push_back( prows2[0] );
+      
+      for (int i = 1; i < prows2.size(); ++i)
+	{
+	  if ( prows2[i] != percolantes.back()) 
+	    percolantes.push_back( prows2[i] );
+	}
+      
+      std::cout << "\n\vlos cluster percolantes son: \n\v";
+      
+      for (int j = 0; j < percolantes.size(); ++j)
+	std::cout << percolantes[j] << "  ";
+      
+      std::cout << "\n\v";
+    }
+  
+  return 0;
+ 
 }
 
 
